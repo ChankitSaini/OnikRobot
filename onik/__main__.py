647a88413373fb149a93a4cc6500b5fb5d88f44d
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-09-10 21:36:11 +0530

    VPS RUN

--- onik/__main__.py
+++ onik/__main__.py
@@ -1,15 +1,30 @@
 import asyncio
+import sys
+sys.path.append('..')
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
 from onik.videoplayer import app
 from onik.videoplayer import call_py
+
+PLUGINS = dict(
+    root="onik",
+    include=[
+        "callback"  ,
+        "inline",
+        "sysinfo",
+        "song",
+        "start",
+        "ytsearch",
+        "videoplayer"
+    ]
+)
 
 bot = Client(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="onik"),
+    plugins=PLUGINS),
 )
 
 bot.start()
