b1a46559d733d9bbbb9f21341bfe1139b8ae9553
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-09-10 15:55:41 +0530

    Intial Commit

---
+++ onik/__main__.py
@@ -0,0 +1,22 @@
+import asyncio
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from onik.videoplayer import app
+from onik.videoplayer import call_py
+
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="onik"),
+)
+
+bot.start()
+print("[STATUS]:✅ »» BOT CLIENT STARTED ««")
+app.start()
+print("[STATUS]:✅ »» USERBOT CLIENT STARTED ««")
+call_py.start()
+print("[STATUS]:✅ »» PYTGCALLS CLIENT STARTED ««")
+idle()
+print("[STATUS]:❌ »» BOT STOPPED ««")
