d2bdd10be5c96d8a2b9b5f4c299e6b0ff77a7879
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-09-12 17:17:35 +0530

    Hm

--- onik/sysinfo.py
+++ onik/sysinfo.py
@@ -5,15 +5,14 @@ import uuid
 
 import psutil
 from pyrogram import Client, filters
-
-from config import BOT_USERNAME
+from config import Onik
 from helpers.decorators import sudo_users_only, humanbytes
 from helpers.filters import command
 
 
 # FETCH SYSINFO
 
-@Client.on_message(command(["sysinfo", f"sysinfo@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["sysinfo", f"sysinfo@{Onik.BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def give_sysinfo(client, message):
     splatform = platform.system()
